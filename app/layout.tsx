b3d7c2d2ea47586bb53823cdec7fa1eab3c82915
Joshardals <joshuabamidele219@gmail.com>
2024-08-21 18:42:26 +0100

    fixed a slight bug

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Nunito_Sans } from "next/font/google";
 import "./globals.css";
 
-export const nunito = Nunito_Sans({
+const nunito = Nunito_Sans({
   subsets: ["latin"],
   weight: ["200", "300", "400", "500", "600", "700", "800", "900", "1000"],
   display: "swap",
