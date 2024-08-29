6b461a0559eb254fe50c6ae5b356745029dfa9db
Joshardals <joshuabamidele219@gmail.com>
2024-08-29 22:07:53 +0100

    initial commit

--- app/(main)/events/payment-success/page.tsx
+++ app/(main)/events/payment-success/page.tsx
@@ -1,44 +1,11 @@
-"use client";
-import { ButtonInput } from "@/components/form/FormInput";
-import {
-  createTicketInfo,
-  updateEventsById,
-} from "@/lib/actions/database.action";
-import { formatPrice } from "@/lib/utils";
-import Link from "next/link";
-import { useSearchParams } from "next/navigation";
-import { useEffect } from "react";
-import { FaRegCircleCheck } from "react-icons/fa6";
+import PaymentSuccess from "@/components/shared/PaymentSuccess";
+
 export default function SuccessPage() {
-  const searchParams = useSearchParams();
-  const amount = searchParams.get("amount");
-  const eventName = searchParams.get("event_name");
-
-  useEffect(() => {
-    const completeTicketProcessing = async () => {
-      const eventId = localStorage.getItem("event_id");
-      if (eventId && eventName) {
-        try {
-          await updateEventsById(eventId);
-          await createTicketInfo({
-            eventName,
-            eventId,
-            price: Number(amount),
-          });
-        } catch (error) {
-          console.log(`An unexpected error occured: ${error}`);
-        } finally {
-          localStorage.removeItem("event_id");
-        }
-      }
-    };
-
-    completeTicketProcessing();
-  }, [amount, eventName]);
-
   return (
     <div className="flex items-center justify-center min-h-screen flex-col space-y-4 max-w-[50rem] mx-auto text-center p-5">
-      <FaRegCircleCheck className="text-emeraldGreen size-10" />
+      <h1>Hey there!</h1>
+      <PaymentSuccess />
+      {/* <FaRegCircleCheck className="text-emeraldGreen size-10" />
       <p className="font-semibold text-pretty">
         Payment Successful! 🎉 Thank you for your purchase. You have
         successfully bought a ticket to <br /> <b>{eventName}</b> for{" "}
@@ -47,7 +14,7 @@ export default function SuccessPage() {
       </p>
       <Link href="/events">
         <ButtonInput variant="ticket" label="Back to Events" />
-      </Link>
+      </Link> */}
     </div>
   );
 }
