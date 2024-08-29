e7db15a65399e4e70223efd604013d9a11e3b305
Joshardals <joshuabamidele219@gmail.com>
2024-08-29 21:18:23 +0100

    initial commit

--- app/(main)/payment-success/page.tsx
+++ app/(main)/payment-success/page.tsx
@@ -1,3 +1,15 @@
+"use client";
+import { ButtonInput } from "@/components/form/FormInput";
+import {
+  createTicketInfo,
+  updateEventsById,
+} from "@/lib/actions/database.action";
+import { formatPrice } from "@/lib/utils";
+import Link from "next/link";
+import { useSearchParams } from "next/navigation";
+import { useEffect } from "react";
+import { FaRegCircleCheck } from "react-icons/fa6";
+
 export default function PaymentSuccessPage() {
   return <div>page</div>;
 }
